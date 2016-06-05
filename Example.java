1f6fd9ae90d27bce6c12b6c5d1ad0609bd07afd4
thomaslian <thomaslian1995@gmail.com>
2016-06-05 14:53:04 +0200

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,36 +1,55 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 import com.teamtreehouse.Treet;
 import com.teamtreehouse.Treets;
 
-import java.util.Arrays;
-import java.util.Date;
 
 public class Example {
-  
-  public static void main(String[] args){
-    Treet treet = new Treet(
-      "craigsdennis",
-      "Want to be famous?",
-      new Date(1421849732000L)
-    );
-    Treet secondTreet = new Treet(
-      "journeytocode",
-      "@treehouse makes learning Java sooooo fun! #treet",
-      new Date(1421878767000L)
-    );
-    System.out.printf("This is a new Treet: %s %n", treet);
-    System.out.println("The words are:");
-    for (String word: treet.getWords()){
-      System.out.println(word);
+
+  public static void main(String[] args) {
+    Treet[] treets = Treets.load();
+    System.out.printf("There are %d treets. %n",
+                     treets.length);
+    Set<String> allHashTags = new HashSet<String>();
+    Set<String> allMentions = new TreeSet<String>();
+    for (Treet treet : treets) {
+      allHashTags.addAll(treet.getHashTags());
+      allMentions.addAll(treet.getMentions());
+  }
+  System.out.printf("Hash tags: %s %n", allHashTags);
+  System.out.printf("Mentions: %s %n", allMentions);
+    
+    Map<String, Integer> hashTagCounts = new HashMap<String, Integer>();
+    for (Treet treet:treets){
+     for (String hashTag : treet.getHashTags()){
+       Integer count = hashTagCounts.get(hashTag);
+       if (count==null){
+         count = 0;
+       }
+       count++;
+       hashTagCounts.put(hashTag, count);
+     }
     }
-    Treet[] treets = {treet, secondTreet};
-    Arrays.sort(treets);
-    for (Treet exampleTreet : treets) {
-      System.out.println(exampleTreet);
+    System.out.printf("Hash tag counts: %s %n", hashTagCounts);
+    
+    Map<String, List<Treet>> treetsByAuthor = new HashMap<String, List<Treet>>();
+    for (Treet treet : treets) {
+      List<Treet> authoredTreets = treetsByAuthor.get(treet.getAuthor());
+      if (authoredTreets == null) {
+       authoredTreets = new ArrayList<Treet>();
+        treetsByAuthor.put(treet.getAuthor(), authoredTreets);
+      }
+      authoredTreets.add(treet);
     }
-    Treets.save(treets);
-    Treet[] reloadedTreets = Treets.load();
-    for (Treet reloaded : reloadedTreets){
-     System.out.println(reloaded);
-    }
+    System.out.printf("Treets by author: %s %n ", treetsByAuthor);
+    System.out.printf("Treets by nickrp: %s %n ", treetsByAuthor.get("nickrp"));
   }
 }
